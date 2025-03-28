cae0e61a59e9084385459564cff6258619579856
ShahzaibSoftwareEngineer <infoshahzaibmunir@gmail.com>
2025-03-28 09:10:37 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def calculator(a, b, op):
+    if op == '+': return a + b
+    if op == '-': return a - b
+    if op == '*': return a * b
+    if op == '/': return a / b
+print(calculator(10, 5, '+'))
